41d407365aa44cc65f540604b1f1d63590384b64
Hayun <jhayoon1997@gmail.com>
2023-11-14 11:20:08 +0900

    Scene update

---
+++ Assets/Scripts/Persistant/ControllerPersistant.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPersistant : MonoBehaviour
+{
+     public float gameRule_gridSize = 2f;
+}
